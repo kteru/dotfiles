ff9e6f9367f07c3ea58e28361ef879148315b1ae
teru <teru@kteru.net>
2024-06-26 20:35:14 +0900

    Improve preview

--- .zshrc.d/fzf-ssh-host.zsh
+++ .zshrc.d/fzf-ssh-host.zsh
@@ -1,9 +1,14 @@
 function fzf-ssh-host() {
   WORD="${LBUFFER##* }"
 
-  item=$( (
-    cat ~/.ssh/config | sed -ne 's/^Host  *//p' | sed -e 's/  */\n/g' | grep -v '*' | sort
-  ) | fzf --query "${WORD}" --preview "cat ~/.ssh/config | grep -v -E '^(\s*|#.*)$' | sed -E -ne '/^Host( *| .*) {}( *| .*)$/,/^[^ ]/p' | sed -e '\$d'")
+  item=$(
+    (
+      awk '/^Host / && !/\*/ { for (i=2; i<=NF; i++) print NR, $i }' ~/.ssh/config | \
+      sort -k 2
+    ) | \
+    fzf --query "${WORD}" --with-nth 2.. --preview-window +{1}/4 --preview "grep --color=always -E '^Host(\s*|\s.*)\s{2}(\s*|\s.*)$|^' ~/.ssh/config" | \
+    cut -d ' ' -f 2-
+  )
 
   if [ -z "${item}" ]; then
     return 0

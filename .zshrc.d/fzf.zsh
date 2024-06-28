092bac7855bdf9da0432aa3d67b622b501b8b7be
teru <teru@kteru.net>
2024-06-28 09:11:18 +0900

    Remove `--no-sort`

--- .zshrc.d/fzf.zsh
+++ .zshrc.d/fzf.zsh
@@ -1,1 +1,1 @@
-export FZF_DEFAULT_OPTS="--layout reverse --no-mouse --cycle --no-sort --bind ctrl-j:jump --bind ctrl-t:toggle-all --bind ctrl-p:toggle-preview --bind ctrl-w:toggle-preview-wrap"
+export FZF_DEFAULT_OPTS="--layout reverse --no-mouse --cycle --bind ctrl-j:jump --bind ctrl-t:toggle-all --bind ctrl-p:toggle-preview --bind ctrl-w:toggle-preview-wrap"

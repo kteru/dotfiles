514b3a52f69213ad1fdd563ee0318e6c0ab6a259
teru <teru@kteru.net>
2025-02-21 16:11:37 +0900

    Change fzf binds

--- .zshrc.d/fzf.zsh
+++ .zshrc.d/fzf.zsh
@@ -1,1 +1,1 @@
-export FZF_DEFAULT_OPTS="--layout reverse --no-mouse --cycle --bind ctrl-j:jump --bind ctrl-t:toggle-all --bind ctrl-p:toggle-preview --bind ctrl-w:toggle-preview-wrap"
+export FZF_DEFAULT_OPTS="--layout reverse --no-mouse --cycle --bind alt-j:jump --bind alt-t:toggle-all --bind alt-p:toggle-preview --bind alt-w:toggle-preview-wrap"
